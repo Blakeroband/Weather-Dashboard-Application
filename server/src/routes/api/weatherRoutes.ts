202f75e13a4836bc4e265903022dcee401ef8405
B A <blakeroband@gmail.com>
2025-03-08 11:27:46 -0600

    server bug with port 3001, instance already running somewhere but i call BS

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,50 @@
 import { Router } from 'express';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+router.post('/', async (req, res) => {
+  try {
+    const { city } = req.body;
+    if (!city) {
+      return res.status(400).send('City name is required');
+    }
+    
+    // Save city to history
+    const savedCity = await HistoryService.addCity(city);
+    
+    // Get weather data for city
+    const weatherData = await WeatherService.getWeatherForCity(city);
+    
+    // Return both the saved city and weather data
+    return res.status(200).json({
+      city: savedCity,
+      weather: weatherData
+    });
+  } catch (error) {
+    console.error('Error processing weather request:', error);
+    return res.status(500).send('Error processing request');
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, res) => {
+  try {
+    const history = await HistoryService.getCities();
+    res.status(200).send(history);
+  } catch (error) {
+    res.status(500).send('Error retrieving history');
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+// router.delete('/history/:id', async (req, res) => {
+
+// });
 
 export default router;
